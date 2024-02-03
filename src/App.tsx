a7d968c87f009960c600166e3d3668a81b35b87c
Christian Onoh <chibyk5000@gmail.com>
2024-02-03 11:31:47 +0100

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, Grid, Show, Text } from "@chakra-ui/react"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Grid templateAreas={
+        {
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`
+        }
+      } height="100vh">
+        <Box gridArea="nav" bg="tomato" p={4}>
+          <Text fontSize="xl" color="white">Header</Text>
+        </Box>
+        <Box gridArea="main" bg="papayawhip" p={4}>
+          <Text fontSize="xl">Main</Text>
+        </Box>
+        <Show above="lg">
+          <Box gridArea="aside" bg="lightgreen" p={4}>
+            <Text fontSize="xl">Aside</Text>
+          </Box>
+        </Show>
+      </Grid>
     </>
   )
 }
