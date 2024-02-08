a810f87e531d673fbd04af62986e7703bf6cdd15
Christian Onoh <chibyk5000@gmail.com>
2024-02-08 11:29:28 +0100

    Modify the platforms and genre hooks to use static data

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,4 @@
-import useData from "./useData";
+import platforms from '../data/parent_platforms'
 
 interface Platform {
   id: number;
@@ -7,6 +7,6 @@ interface Platform {
   image_background: string;
 }
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+const usePlatforms = () => ({data: platforms, isLoading: false, error: null});
 
 export default usePlatforms;
